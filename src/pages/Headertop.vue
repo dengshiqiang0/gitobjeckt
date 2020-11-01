aeb68d82595b26ab0c88b45c17b32fd8b1f82acc
邓世强组长 <958988329@qq.com>
2020-11-01 15:10:37 +0800

    第一个功能完成

--- src/pages/Headertop.vue
+++ src/pages/Headertop.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+      <div>
+          我是项目的第一个功能,已经完成了
+      </div>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Headertop from "../pages/Headertop.vue"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // }
+    {
+      path: '/',
+      name: 'Headertop',
+      component: Headertop
+    }
   ]
 })
